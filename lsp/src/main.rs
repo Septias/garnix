cb120e3aa88952f7fa06e53df59adbd7b4374459
Sebastian Klähn <scoreplayer2000@gmail.com>
2024-03-27 13:23:45 +0100

    fix lsp

--- lsp/src/main.rs
+++ lsp/src/main.rs
@@ -6,8 +6,8 @@ use async_lsp::{
 use lsp_types::{
     notification,
     request::{self, DocumentDiagnosticRequest, InlayHintRequest},
-    Diagnostic, Hover, HoverContents, HoverProviderCapability, InitializeResult, InlayHintLabel,
-    InlayHintServerCapabilities, MarkedString, ServerCapabilities, Url,
+    Diagnostic, DocumentDiagnosticReport, FullDocumentDiagnosticReport, HoverProviderCapability,
+    InitializeResult, InlayHintLabel, RelatedFullDocumentDiagnosticReport, ServerCapabilities, Url,
 };
 use std::{collections::HashMap, fs, ops::ControlFlow};
 use tower::ServiceBuilder;
@@ -32,12 +32,12 @@ impl ServerState {
             Ok(ast) => {
                 let ast = infer::Ast::from_parser_ast(ast, &content);
                 if let Err(e) = infer(&ast) {
-                    if !self.errors.contains_key(&uri.as_str()) {
+                    if !self.errors.contains_key(uri.as_str()) {
                         let errors = self.errors.get_mut(uri.as_str()).unwrap();
                         errors.push(Diagnostic {
                             range: to_lsp_range(&e.span, &content),
                             message: format!("{:?}", e),
-                            severity: Some(lsp_types::DiagnosticSeverity::Error),
+                            severity: Some(lsp_types::DiagnosticSeverity::ERROR),
                             ..Default::default()
                         });
                     } else {
@@ -46,34 +46,32 @@ impl ServerState {
                             vec![Diagnostic {
                                 range: to_lsp_range(&e.span, &content),
                                 message: format!("{:?}", e),
-                                severity: Some(lsp_types::DiagnosticSeverity::Error),
+                                severity: Some(lsp_types::DiagnosticSeverity::ERROR),
                                 ..Default::default()
                             }],
                         );
                     }
                 };
+                self.files.insert(uri.as_str().to_string(), (ast, content));
             }
             Err(err) => {
                 self.errors.insert(uri.to_string(), vec![]);
-                let errors = self.errors.get_mut(uri.as_str()).unwrap();
+                let error_store = self.errors.get_mut(uri.as_str()).unwrap();
                 match err {
                     parser::ParseError::MultipleErrors { errors } => {
                         for (kind, span) in errors {
-                            let diagnostic = Diagnostic {
+                            error_store.push(Diagnostic {
                                 range: to_lsp_range(&span, &content),
                                 message: format!("{:?}", kind),
-                                severity: Some(lsp_types::DiagnosticSeverity::Error),
+                                severity: Some(lsp_types::DiagnosticSeverity::ERROR),
                                 ..Default::default()
-                            };
-                            errors.push(diagnostic);
+                            });
                         }
                     }
                 }
-                errors.extend(err)
             }
         };
 
-        self.files.insert(uri.as_str().to_string(), (ast, content));
         Ok(())
     }
 }
@@ -94,94 +92,56 @@ async fn main() {
                     capabilities: ServerCapabilities {
                         hover_provider: Some(HoverProviderCapability::Simple(true)),
                         inlay_hint_provider: Some(lsp_types::OneOf::Left(true)),
-                        diagnostic_provider: Some(true),
+                        diagnostic_provider: Some(
+                            lsp_types::DiagnosticServerCapabilities::Options(
+                                lsp_types::DiagnosticOptions {
+                                    ..Default::default()
+                                },
+                            ),
+                        ),
                         ..ServerCapabilities::default()
                     },
                     server_info: None,
                 })
             })
-            .request::<request::HoverRequest, _>(|st, a| {
-                let ast = st
-                    .files
-                    .get(a.text_document_position_params.text_document.uri.as_str());
-
-                let ret = if let Some((ast, source)) = ast {
-                    let info: Option<(String, &std::ops::Range<usize>)> = (move || {
-                        let position =
-                            from_lsp_position(a.text_document_position_params.position, source);
-                        let node = ast.get_node_at(position)?;
-
-                        let name = node.as_ref();
-                        let constraints = node
-                            .get_identifier()
-                            .ok()
-                            .map(|ident| ident.get_constraints());
-                        let ty = node.get_identifier().ok().map(|ident| ident.get_type());
-
-                        let mut ret = format!("Node: `{:?}`\n", name);
-                        if let Some(constraints) = constraints {
-                            ret.push_str(&format!("Constraints: `{:?}`\n", constraints));
-                        }
-                        if let Some(ty) = ty {
-                            ret.push_str(&format!("Type: `{:?}`\n", ty));
-                        }
-
-                        Some((ret, ast.get_span()))
-                    })();
-
-                    Ok(info.map(|(info, span)| Hover {
-                        contents: HoverContents::Scalar(MarkedString::String(info)),
-                        range: Some(to_lsp_range(span, source)),
-                    }))
-                } else {
-                    st.load_file(&a.text_document_position_params.text_document.uri)
-                        .ok();
-                    eprintln!("loaded file: {:?}", st.files);
-                    Ok(Some(Hover {
-                        contents: HoverContents::Scalar(MarkedString::String(format!(
-                            "file not yet read {}",
-                            a.text_document_position_params.text_document.uri
-                        ))),
-                        range: None,
-                    }))
-                };
-                async move { ret }
-            })
             .request::<InlayHintRequest, _>(|st, params| {
                 let ret = st
                     .files
-                    .get(&params.text_document.uri)
+                    .get(params.text_document.uri.as_str())
                     .map(|(ast, source)| {
                         let idents = ast.collect_identifiers();
-                        idents.into_iter().map(|ident| lsp_types::InlayHint {
-                            position: to_lsp_range(ast.get_span(), source),
-                            label: InlayHintLabel::String(ident.ty),
-                            ..Default::default()
-                        });
-
-                        hints
+                        idents
+                            .into_iter()
+                            .map(|ident| lsp_types::InlayHint {
+                                position: to_lsp_range(ast.get_span(), source).start,
+                                label: InlayHintLabel::String(format!("{:?}", ident.var)),
+                                kind: None,
+                                text_edits: None,
+                                tooltip: None,
+                                padding_left: None,
+                                padding_right: None,
+                                data: None,
+                            })
+                            .collect()
                     });
-                async move { ret }
+                async move { Ok(ret) }
             })
-            .request::<DocumentDiagnosticRequest>(|st, params| {
+            .request::<DocumentDiagnosticRequest, _>(|st, params| {
                 let ret = st
-                    .files
-                    .get(&params.text_document.uri)
-                    .map(|(ast, source)| {
-                        let mut diagnostics = vec![];
-                        for error in ast.errors.iter() {
-                            let range = to_lsp_range(&error.span, source);
-                            let diagnostic = Diagnostic {
-                                range,
-                                message: error.message.clone(),
-                                severity: Some(lsp_types::DiagnosticSeverity::Error),
-                                ..Default::default()
-                            };
-                            diagnostics.push(diagnostic);
-                        }
-                        diagnostics
-                    });
-                async move { ret }
+                    .errors
+                    .remove(params.text_document.uri.as_str())
+                    .unwrap_or_default();
+                async move {
+                    Ok(lsp_types::DocumentDiagnosticReportResult::Report(
+                        DocumentDiagnosticReport::Full(RelatedFullDocumentDiagnosticReport {
+                            related_documents: None,
+                            full_document_diagnostic_report: FullDocumentDiagnosticReport {
+                                result_id: None,
+                                items: ret,
+                            },
+                        }),
+                    ))
+                }
             })
             .notification::<notification::Initialized>(|_, _| ControlFlow::Continue(()))
             .notification::<notification::DidChangeConfiguration>(|_, _| ControlFlow::Continue(()))
@@ -228,42 +188,4 @@ async fn main() {
     );
 
     server.run_buffered(stdin, stdout).await.unwrap();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_to_lsp_range() {
-        let source = "aaaaaa\nbeeeeeee\nceeeeee";
-        assert_eq!(
-            to_lsp_range(&parser::Span { start: 11, end: 13 }, source),
-            lsp_types::Range {
-                start: Position {
-                    line: 2,
-                    character: 5
-                },
-                end: Position {
-                    line: 2,
-                    character: 7
-                }
-            }
-        );
-    }
-
-    #[test]
-    fn test_from_lsp_position() {
-        let source = "aaaaaa\nbeeeeeee\nceeeeee";
-        assert_eq!(
-            from_lsp_position(
-                Position {
-                    line: 1,
-                    character: 5
-                },
-                source
-            ),
-            12
-        );
-    }
 }
--- lsp/src/utils.rs
+++ lsp/src/utils.rs
@@ -29,7 +29,7 @@ pub(crate) fn to_lsp_range(span: &parser::Span, source: &str) -> lsp_types::Rang
     }
 }
 
-pub(crate) fn from_lsp_position(pos: Position, source: &str) -> usize {
+pub(crate) fn _from_lsp_position(pos: Position, source: &str) -> usize {
     let start_offset = source
         .lines()
         .take(pos.line as usize)
@@ -52,5 +52,43 @@ fn _from_lsp_range(range: &lsp_types::Range, source: &str) -> parser::Span {
     parser::Span {
         start: start_offset + range.start.character as usize,
         end: end_offset + range.end.character as usize,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_to_lsp_range() {
+        let source = "aaaaaa\nbeeeeeee\nceeeeee";
+        assert_eq!(
+            to_lsp_range(&parser::Span { start: 11, end: 13 }, source),
+            lsp_types::Range {
+                start: Position {
+                    line: 2,
+                    character: 5
+                },
+                end: Position {
+                    line: 2,
+                    character: 7
+                }
+            }
+        );
+    }
+
+    #[test]
+    fn test_from_lsp_position() {
+        let source = "aaaaaa\nbeeeeeee\nceeeeee";
+        assert_eq!(
+            _from_lsp_position(
+                Position {
+                    line: 1,
+                    character: 5
+                },
+                source
+            ),
+            12
+        );
     }
 }
