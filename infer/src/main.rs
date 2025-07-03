4ebb48f63f8fa8ff9744effba2c9886747573c47
Septias <scoreplayer2000@gmail.com>
2025-07-03 15:45:04 +0200

    eprint

--- infer/src/main.rs
+++ infer/src/main.rs
@@ -6,7 +6,6 @@ fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if let Some(filename) = &args.get(1) {
         let input = std::fs::read_to_string(filename)?;
-        println!("input: {}", input);
         let tokens = lex(&input);
         match parser::parser::expr(NixTokens(&tokens)) {
             Ok((_, ast)) => {
@@ -14,20 +13,20 @@ fn main() -> anyhow::Result<()> {
                 println!("ast: {:#?}", ast);
                 match infer::coalesced(&ast) {
                     Err(e) => {
-                        println!("[Inference] Error: {:?}", e);
+                        eprintln!("[Inference] Error: {:?}", e);
                     }
                     Ok(ty) => println!("Inferred type: {:?}", ty.show()),
                 };
             }
             Err(e) => match e {
                 err @ nom::Err::Error(_) | err @ nom::Err::Failure(_) => {
-                    println!("[Parser] Error: {:?}", map_err(err))
+                    eprintln!("[Parser] Error: {:?}", map_err(err))
                 }
                 nom::Err::Incomplete(e) => println!("{:?}", e),
             },
         }
     } else {
-        println!("You have to supply a filename")
+        eprintln!("You have to supply a filename")
     }
     Ok(())
 }
